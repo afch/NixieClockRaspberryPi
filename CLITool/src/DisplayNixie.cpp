d48d6134530d0b2f7ff3ee1a2c7388d7154cd296
Aleksej Fomin <fominalec@gmail.com>
2019-01-18 01:01:22 +0200

    v. 1.2
    Added: NCS314-8C (9 tubes support)

--- CLITool/src/DisplayNixie.cpp
+++ CLITool/src/DisplayNixie.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : DisplayNixie.cpp
 // Author      : GRA&AFCH
-// Version     :
+// Version     : v1.2
 // Copyright   : Free
 // Description : Display digits on shields
 //============================================================================
@@ -17,6 +17,8 @@ int LEpin=3;
 uint16_t SymbolArray[10]={1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
 
 int main(int argc, char* argv[]) {
+	printf("Display Nixie CLI Tool v1.2\n\r");
+	//printf("Arg len: %d \n\r", strlen(argv[1]));
 	if (argc < 2)
 	{
 		printf("Enter digits to display... or commands: now - present time, clock - loop program");
@@ -24,12 +26,13 @@ int main(int argc, char* argv[]) {
 	}
 	wiringPiSetup ();
 
-	if (wiringPiSPISetupMode (0, 2000000, 2)) printf("SPI ok");
-			else {printf("SPI NOT ok"); return 0;}
+	if (wiringPiSPISetupMode (0, 2000000, 2)) printf("SPI ok\n\r");
+			else {printf("SPI NOT ok\n\r"); return 0;}
 	//char* _argv;
 	//String _stringToDisplay=argv[1];
-	char _stringToDisplay[8];
+	char _stringToDisplay[10];
 	bool clock=0;
+	int tubes_qty=0;
 
 	do {
 	if (!strcmp(argv[1],"now")) //printf("NOWW"); else printf("NOTTT NOWW");
@@ -52,7 +55,13 @@ int main(int argc, char* argv[]) {
 
 	else
 	{
-		for (int i=0; i<8; i++)
+		tubes_qty=strlen(argv[1]);
+		if ((tubes_qty != 6) && (tubes_qty != 9))
+		{
+			puts("Wrong length: must be 6 or 9 digits");
+			return 0;
+		}
+		for (int i=0; i<tubes_qty+1; i++)
 		{
 			_stringToDisplay[i]=argv[1][i];
 		}
@@ -60,18 +69,20 @@ int main(int argc, char* argv[]) {
 
 	pinMode(LEpin, OUTPUT);
 
-	uint32_t Var32=0;
+	uint32_t Var32;
+	unsigned char buff[12];
+
+	/*Var32=0;
 
 	Var32= (SymbolArray[_stringToDisplay[5]-0x30])<<20;
 	Var32|=(SymbolArray[_stringToDisplay[4]-0x30])<<10;
 	Var32|=(SymbolArray[_stringToDisplay[3]-0x30]);
 
-	unsigned char buff[8];
 	buff[0]=Var32>>24;
 	buff[1]=Var32>>16;
 	buff[2]=Var32>>8;
 	buff[3]=Var32;
-
+	// HV #2
 	Var32=0;
 
 	Var32= (SymbolArray[_stringToDisplay[2]-0x30])<<20;
@@ -82,9 +93,30 @@ int main(int argc, char* argv[]) {
 	buff[5]=Var32>>16;
 	buff[6]=Var32>>8;
 	buff[7]=Var32;
+	//HV#1 */
+
+	int bufferIndex=0;
+
+	for (int i=0; i<tubes_qty+1; i=i+3)
+	{
+		Var32=0;
+
+		Var32= (SymbolArray[_stringToDisplay[tubes_qty-i-1]-0x30])<<20;
+		Var32|=(SymbolArray[_stringToDisplay[tubes_qty-i-2]-0x30])<<10;
+		Var32|=(SymbolArray[_stringToDisplay[tubes_qty-i-3]-0x30]);
+
+		buff[bufferIndex]=Var32>>24;
+		bufferIndex++;
+		buff[bufferIndex]=Var32>>16;
+		bufferIndex++;
+		buff[bufferIndex]=Var32>>8;
+		bufferIndex++;
+		buff[bufferIndex]=Var32;
+		bufferIndex++;
+	}
 
 	digitalWrite(LEpin, LOW);
-	wiringPiSPIDataRW(0, buff, 8);
+	wiringPiSPIDataRW(0, buff, 4*tubes_qty/3);
 	digitalWrite(LEpin, HIGH);
 	} while (clock);
 	return 0;
